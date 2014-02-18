96a18729d0b12adf2c904a067478f530a60fa4c3
Francisco Neves <fntneves@gmail.com>
2014-02-18 17:55:43 +0000

    Add Projects & Editions pages

--- full_template/assets/scripts/main-scripts.js
+++ full_template/assets/scripts/main-scripts.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	// Parallax Main-Header
 	$(window).scroll( function()
-    {
+	{
 		var scroll = $(window).scrollTop(), slowScroll = scroll/2;
-		$('#home-header > .container').css({ transform: "translateY(" + slowScroll + "px)" });
+		$('.page-header > .container').css({ transform: "translateY(" + slowScroll + "px)" });
 	});
 
 });
