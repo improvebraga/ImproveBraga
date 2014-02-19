f949abf43968456ab05a29231dabb36ec616f056
Francisco Neves <fntneves@gmail.com>
2014-02-19 22:39:44 +0000

    Add Project Details Page

--- full_template/assets/scripts/main-scripts.js
+++ full_template/assets/scripts/main-scripts.js
@@ -7,8 +7,21 @@ document.addEventListener('DOMContentLoaded', function(){
 	// Parallax Main-Header
 	$(window).scroll( function()
 	{
-		var scroll = $(window).scrollTop(), slowScroll = scroll/2;
-		$('.page-header > .container').css({ transform: "translateY(" + slowScroll + "px)" });
+		var scroll = $(window).scrollTop(), slowScroll = scroll/2
+		$('.page-header > .container').css({ transform: "translateY(" + slowScroll + "px)" })
 	});
+
+});
+
+$(document).ready(function(){
+
+	// Add Carousel
+	$('.carousel').carousel()
+
+	// Add Tabs
+	$('#myTab a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
 
 });
